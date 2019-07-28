b43725ded4d5c717bd9df9ca2291ecd4c2a9ac68
Washington Alexandre <washington.silva@totvs.com.br>
2019-07-28 12:44:00 -0300

    #adicionado documentação

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,1 +1,10 @@
-export const imports = {}
+export const imports = {
+  'src/docs/documentation/Home.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-docs-documentation-home" */ 'src/docs/documentation/Home.mdx'
+    ),
+  'src/docs/documentation/document-settings.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-docs-documentation-document-settings" */ 'src/docs/documentation/document-settings.mdx'
+    ),
+}
---
+++ doczrc.js
@@ -0,0 +1,9 @@
+export default {
+    themeConfig: {
+      mode: 'dark',
+      logo: {
+        src: '/path/of/my/logo',
+        width: 150,
+      },
+    },
+  }
