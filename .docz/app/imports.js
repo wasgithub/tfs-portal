b00e45c6cd67c3a3192e6922ebd710ca14655f9c
Washington Alexandre <washington.silva@totvs.com.br>
2019-07-29 14:02:14 -0300

    Adicionado arquivo de desafio

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,4 +1,8 @@
 export const imports = {
+  'src/docs/documentation/challenge.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-docs-documentation-challenge" */ 'src/docs/documentation/challenge.mdx'
+    ),
   'src/docs/documentation/document-settings.mdx': () =>
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-docs-documentation-document-settings" */ 'src/docs/documentation/document-settings.mdx'
